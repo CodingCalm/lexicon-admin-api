43e6ae27c8b4a88be7039bdfda410f970c73328e
Henrik <henrikekonomi@gmail.com>
2024-11-26 15:46:17 +0100

    ändrat till Key Vault

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using lexicon_admin.api.Data;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,22 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+var keyVaultName = "lexicon-vault"; 
+var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net");
+
+builder.Configuration.AddAzureKeyVault(
+    keyVaultUri,
+    new DefaultAzureCredential());
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration["lexicon-connectionstring"]));
+
+
 
 
 var app = builder.Build();
