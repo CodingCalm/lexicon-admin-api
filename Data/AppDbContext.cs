90f50f09ec8e7a6f268d35a725c38296d49fa6f8
Henrik <henrikekonomi@gmail.com>
2024-11-26 10:06:51 +0100

    firstinit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using lexicon_admin.api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace lexicon_admin.api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+}
---
+++ Models/Entities/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace lexicon_admin.api.Models.Entities;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
